17de4a549e73259cbee0c9e7b14a5d2ff283d12f
Garri <garri.ribeiro@gmail.com>
2020-09-06 13:54:49 -0300

    :heavy_plus_sign: Add @react-spectrum and normalize css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
 import React from "react";
+import 'normalize.css';
 
-const App = () => <div>Hello perhero!</div>;
+import { Provider, defaultTheme, Content } from '@adobe/react-spectrum';
+
+const App = () => (
+  <Provider theme={defaultTheme}>
+    <Content>
+        Hello perhero with react-spectrum
+    </Content>
+  </Provider>
+);
 
 export default App;
